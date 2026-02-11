0549a6e4348cd72bd3729a2504badde48a1cecfa
MahmoudALamMohamed <mahmoudalm2019@gmail.com>
2026-02-11 17:27:11 +0200

    edit

--- src/lib/bunny.ts
+++ src/lib/bunny.ts
@@ -7,7 +7,7 @@ const CDN_HOSTNAME = 'menuss.b-cdn.net';
 
 const COVER_COMPRESSION_OPTIONS = {
   maxSizeMB: 3,
-  maxWidthOrHeight: 2800,
+  maxWidthOrHeight: 3500,
   useWebWorker: true,
   fileType: 'image/webp' as const,
   initialQuality: 0.95,
