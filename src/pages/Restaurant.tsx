18f5148ebe87bf24f1dcde09ce2aad8943e0d1e7
MahmoudALamMohamed <mahmoudalm2019@gmail.com>
2026-02-12 17:06:43 +0200

    edit cover

--- src/pages/Restaurant.tsx
+++ src/pages/Restaurant.tsx
@@ -397,7 +397,7 @@ ${orderText}
       {/* Cover Image - صورة الغلاف (img tag للـ blur بدل background-image لتجنب طلب مزدوج) */}
       <div className="relative w-full h-56 sm:h-64 md:h-80 lg:h-96 overflow-hidden">
         {restaurant.cover_image_url && (
-          <div className="relative w-full h-full flex items-center justify-center z-10 px-2">
+          <div className="relative w-full h-full flex items-center justify-center z-10 p-2">
             <img 
               src={getCoverImageUrl(restaurant.cover_image_url)} 
               alt={restaurant.name} 
